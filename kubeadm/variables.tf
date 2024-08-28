ae8aedea2e10298332f1ea17c8e4f24e0e4424f9
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2024-08-28 12:26:28 +0530

    Update variables.tf

--- kubeadm/variables.tf
+++ kubeadm/variables.tf
@@ -10,29 +10,9 @@ variable "pve_node_name" {
   default     = "pve"
 }
 
-variable "machine_type" {
-  type        = string
-  description = "The machine type to deploy to"
-  default     = "e2-medium"
-}
-
-variable "image" {
-  type        = map(string)
-  description = "The image to deploy to"
-  default     = {
-     centos   = "projects/centos-cloud/global/images/centos-stream-8-v20240110"
-     ubuntu   = "projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20240209"
-  }
-}
-
 variable "user" {
   type    = string
   default = "cloudcafe"
-}
-
-variable "email" {
-  type    = string
-  default = "test@gmail.com"
 }
 
 variable "privatekeypath" {
@@ -78,24 +58,19 @@ variable "k8s_master03_ip" {
 variable "k8s_worker01_ip" {
   type        = string
   description = "IP address of k8s worker 01"
-  default = "192.168.29.217"
+  default = "192.168.29.227"
 }
 
 variable "k8s_worker02_ip" {
   type        = string
   description = "IP address of k8s worker 02"
-  default = "192.168.29.218"
+  default = "192.168.29.228"
 }
 
 variable "k8s_worker03_ip" {
   type        = string
   description = "IP address of k8s worker 03"
-  default = "192.168.29.219"
-}
-
-variable "storage_pool_name" {
-  type        = string
-  description = "Name of the storage pool you want to use to store the VM disk"
+  default = "192.168.29.229"
 }
 
 variable "subnet_gw" {
@@ -103,5 +78,3 @@ variable "subnet_gw" {
   description = "IP address of gateway"
   default = "192.168.29.1"
 }
-
-
