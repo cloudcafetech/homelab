73cf45ec39999c5336d44f4d99d58d56167c3519
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2024-08-28 14:27:14 +0530

    Update k8s-ad-enable.tf

--- rke2/k8s-ad-enable.tf
+++ rke2/k8s-ad-enable.tf
@@ -14,10 +14,10 @@ resource "null_resource" "k8s-ad-ldap-integration" {
     }
     provisioner "remote-exec" {
       inline = [
-        "export lbpubip=${var.k8s-haproxy-lb}",
-        "export lbpriip=${var.k8s-haproxy-lb}",
+        "export lbpubip=${var.k8s_haproxy_lb}",
+        "export lbpriip=${var.k8s_haproxy_lb}",
         "export ldapip=${var.ldapip}",
-        "#export ldapip=${var.k8s-haproxy-lb}",
+        "#export ldapip=${var.k8s_haproxy_lb}",
         "sh ./k8setup.sh adauth",
       ]
     }
@@ -28,12 +28,12 @@ resource "null_resource" "k8s-api-alter-master1" {
     connection {
       type        = "ssh"
       user        = var.user
-      host        = var.k8srke2-master-1.network_interface[0].network_ip
+      host        = var.k8s_master01_ip
       private_key = file(var.privatekeypath)
     }
     provisioner "remote-exec" {
       inline = [
-        "export lbpubip=${var.k8s-haproxy-lb.network_interface[0].access_config[0].nat_ip}",
+        "export lbpubip=${var.k8s_haproxy_lb}",
         "sh ./k8setup.sh apialter",
       ]
     }
@@ -44,12 +44,12 @@ resource "null_resource" "k8s-api-alter-master2" {
     connection {
       type        = "ssh"
       user        = var.user
-      host        = var.k8srke2-master-2.network_interface[0].network_ip
+      host        = var.k8s_master02_ip
       private_key = file(var.privatekeypath)
     }
     provisioner "remote-exec" {
       inline = [
-        "export lbpubip=${var.k8s-haproxy-lb.network_interface[0].access_config[0].nat_ip}",
+        "export lbpubip=${var.k8s_haproxy_lb}",
         "sh ./k8setup.sh apialter",
       ]
     }
