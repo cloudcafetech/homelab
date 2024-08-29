3e2db017e8d59fe927fe6977439768743b251932
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2024-08-29 20:18:29 +0530

    Update ocp-bootstrap.tf

--- ocp/ocp-bootstrap.tf
+++ ocp/ocp-bootstrap.tf
@@ -1,26 +1,32 @@
 resource "proxmox_vm_qemu" "bootstrap" {
-  depends_on = [ jumphost ]
 
-  name                      = "var.bootstrap_hn"
+  name                      = var.bootstrap_hn
   target_node               = var.pve_node_name
   vmid                      = var.ocp_bootstrap_ip
-  cpu                       = "host"  
+  cpu                       = "host"
   agent                     = 0
   cores                     = 4
   memory                    = 6114
-  boot                      = "order=scsi0;ide2;net0" 
+  boot                      = "order=scsi0;ide2;net0"
+  pxe                       = true
   onboot                    = true
   scsihw                    = "virtio-scsi-pci"
   bootdisk                  = "scsi0"
-  disk {
-    size    = "40G"
-    type    = "scsi"
-    storage = "local-lvm"
-    #iothread = 1
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          discard = true
+          emulatessd = true
+          storage = "local-lvm"
+          size = 40
+        }
+      }
+    }
   }
   network {
-    model   = "virtio"
-    bridge  = vmbr0
+    bridge = "vmbr0"
+    model = "virtio"
     macaddr = var.ocp_bootstrap_mac
   }
 }
