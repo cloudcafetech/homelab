86f4cc35f463018d737f8e349d303da3e1afe614
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2026-01-14 21:26:16 +0530

    Add files via upload

---
+++ ocp/qcow2-to-liveos.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "QCOW file name: $1"
+FN=$1
+
+if [ -z "$FN" ]; then
+    echo "Please enter QCOW file name."
+    exit
+fi
+
+TMPSTORE=/root/store
+ROOTIMG_DIR=/mnt/rootimg
+ROOT_DIR=/mnt/myroot
+
+mkdir -p $TMPSTORE
+mkdir -p $ROOT_DIR
+sudo rm -rf $TMPSTORE/*
+
+if lsblk | grep -q nbd1; then
+    echo "Cleaning up existing /dev/nbd1 connection..."
+    sudo qemu-nbd --disconnect /dev/nbd1
+fi
+
+# Cleanup
+# 
+sudo umount -l /dev/nbd*
+sudo qemu-nbd --disconnect /dev/nbd1
+sudo qemu-nbd --disconnect /dev/nbd0
+
+# Mounting the rootfs
+sudo modprobe nbd max_part=16
+sudo qemu-nbd --connect=/dev/nbd1 $FN
+sudo mount /dev/nbd1p1 $ROOT_DIR
+ROOTFS_DEVICE=/dev/nbd1
+
+
+# Creating the squash FS
+cd $TMPSTORE
+dir_size=$(sudo du -sm $ROOT_DIR | awk '{print $1}')
+img_size=$((dir_size + 200))
+dd if=/dev/zero of=rootfs.img bs=1M count=$img_size
+mkfs.ext4 rootfs.img
+sudo e2label rootfs.img "Anaconda"
+
+
+sudo mkdir -p $ROOTIMG_DIR
+sudo mount -o loop rootfs.img $ROOTIMG_DIR
+sudo cp -a $ROOT_DIR/* $ROOTIMG_DIR
+sudo umount $ROOTIMG_DIR
+mkdir -p LiveOS
+mv rootfs.img LiveOS/
+mksquashfs LiveOS custom_root.squashfs -comp xz
+
+
+# Copying the vmlinuz and initramfs
+cp $ROOT_DIR/boot/vmlinuz-* $TMPSTORE
+cp $ROOT_DIR/boot/initramfs-*.img $TMPSTORE
+
+sudo umount -l /dev/nbd*
+sudo qemu-nbd --disconnect /dev/nbd1
+sudo qemu-nbd --disconnect /dev/nbd0
