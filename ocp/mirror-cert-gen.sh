5c54f203b7e6d4d77ad1497d687f1fcd9df8320f
Cloud Cafe <68260844+cloudcafetech@users.noreply.github.com>
2026-02-17 07:54:11 +0530

    Update mirror-cert-gen.sh

--- ocp/mirror-cert-gen.sh
+++ ocp/mirror-cert-gen.sh
@@ -7,53 +7,49 @@ STATE=WB
 LOCAL=Kolkata
 MAIL=cloudcafe@pkar.tech
 
-echo - Create new root CA and private key
-
-openssl genrsa -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.pem -subj "/C=$COUNTRY/ST=$STATE/L=$LOCAL/O=Quay/OU=Quay/CN=*.$DOMAIN/emailAddress=$MAIL"
-
-echo - Create a private key which will sign the certificate signing request
-
-openssl genrsa -out ssl.key 2048
-
-echo - Create an openssl.cnf file which will be used to apply SAN extensions to both CSR and end certificate
-
 cat << EOF > openssl.cnf
 [req]
+default_bits = 4096
+default_md = sha256
 distinguished_name = req_distinguished_name
-req_extensions = v3_req
+x509_extensions = v3_req
 prompt = no
-
 [req_distinguished_name]
-countryName = $COUNTRY
-stateOrProvinceName = $STATE
-localityName = $LOCAL
-organizationName = Quay
-organizationalUnitName = Quay
-commonName = *.$DOMAIN
-
-[ v3_req ]
-# Extensions to add to a certificate request
-basicConstraints = CA:FALSE
-keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+C = $COUNTRY
+ST = $STATE
+L = $LOCAL
+O = Quay
+OU = Division
+CN = *.$DOMAIN
+[v3_req]
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment, keyCertSign
+extendedKeyUsage = serverAuth
 subjectAltName = @alt_names
-
 [alt_names]
-DNS.1 = *.$DOMAIN
+DNS = *.$DOMAIN
 EOF
 
-echo - Create certificate signing request using openssl.cnf
+echo - Create root CA and private key
+openssl genrsa -out rootCA.key 2048
+openssl req -x509 -new -config openssl.cnf -nodes -key rootCA.key -sha256 -days 3650 -out rootCA.pem -addext basicConstraints=critical,CA:TRUE,pathlen:1
 
-openssl req -new -key ssl.key -out ssl.csr -reqexts v3_req -config openssl.cnf
+echo "Create private key which will sign Certificate Signing Request (CSR)"
+openssl genrsa -out ssl.key 2048
 
-echo - To check the validity of the generated CSR
+echo - Create CSR using openssl.cnf
+openssl req -new -key ssl.key -out ssl.csr -subj "/CN=quay-enterprise" -config openssl.cnf
 
+echo - Validiting generated CSR
 openssl req -in ssl.csr -noout -text
 
-echo - Based on the created CSR, create server certificate
+echo - Create Server certificate
+openssl x509 -req -in ssl.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out ssl.cert -days 3560 -extensions v3_req -extfile openssl.cnf
 
-openssl x509 -req -in ssl.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out ssl.cert -days 6350 -extensions v3_req -extfile openssl.cnf
+echo -  Create Chain Cert
+cat ssl.cert rootCA.pem > chain.cert
 
-echo - Check certificate has the proper extensions added
+echo - Replace ssl cert with chain cert
+mv --force chain.cert ssl.cert
 
+echo - Checking Certificate has the proper extensions added
 openssl x509 -in ssl.cert -noout -text
